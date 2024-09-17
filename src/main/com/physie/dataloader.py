e143ce63e4b3c4ee73ecea2de8de87433a392237
Physiwell Maume <physiwellmaume@gmail.com>
2024-09-17 12:28:50 +0900

    Initial commit for dataloader python coding practice

---
+++ src/main/com/physie/dataloader.py
@@ -0,0 +1,36 @@
+from pyspark.sql import SparkSession
+
+class Dataloader:
+
+    #Create a spark session
+    spark = SparkSession.builder.appName("FileReader").getOrCreate()
+    def __init__(self,file_format,header,inferschema,path):
+        self.format = file_format
+        self.header = header
+        self.inferschema = inferschema
+        self.path = path
+
+    def load_file(self):
+        # global spark
+        df = None #Initialize df to None
+        try:
+            df = self.spark.read.format(self.format) \
+            .option("header", self.header) \
+            .option("inferSchema", self.inferschema) \
+            .load(self.path)
+
+            print(f"File has {df.count()} rows")
+
+            print("Showing the first ten rows in your file")
+            df.show(10)
+
+        except Exception as e:
+            print(f"Your data could not be loaded. Error: {str(e)}")
+            return "Failed to load the file"
+
+def main():
+    app = Dataloader("csv","true","true","/Users/physiwellmaume/Desktop/myProjects/basicDataLoader/data/housing.csv")
+    app.load_file()
+
+if __name__ == "__main__":
+    main()
